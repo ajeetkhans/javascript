f7872a3f44c080453bd72da38b32344918713a42
ajitkhans <ajeetkhan0420@gmail.com>
2023-02-09 17:03:58 +0530

    new file add

---
+++ index.js
@@ -0,0 +1,206 @@
+// for (let a = 1;a<=100;a++){
+//     console.log(a);
+// }
+
+// //
+
+// let b = 1;
+// while(b<=100){
+//     if(b%1===0)
+//     console.log(b);
+//     console.log('odd');
+// {
+//     b++
+// }
+// }
+// // let c = 1
+// // do{
+// //     console.log(c);
+// //     c++;
+// // } while (c<=10)
+// // 
+// for(let d = 1;d<=100;d++){
+//     if(d%2==0)
+//     console.log(d);
+//     console.log('even');
+// }
+// // 4
+
+// for(let e = 1;e<=100;e++){
+//     if(e%5===0)
+//     console.log(e);
+// }
+//  //5
+
+//  for (let f = 1;f<=100;f++){
+//     if(f%5==0&&f%15==0)
+//     console.log(f);
+//  }
+
+//  //6
+
+
+//  for(let g = 1;g<=20;g++){
+//     if(g%2==0)
+//     console.log(g);
+//  }
+//  //7
+ 
+//  let ans = Number( prompt("how many times do you "))
+//  for(let h = 1;h<=ans;h++){
+//     console.log("hello word");
+//  }
+// //8
+
+// let sum = 0
+// for(let i = 0;i<=100;i++)
+//     {
+//         sum= sum +i
+//     }
+// console.log(sum);
+// // 9
+
+
+// let sums = 0
+// for (let j = 0;j<=100;j++){
+//     if(j%1==0){
+//         console.log('odd');
+//         sums=sums+j
+//     }
+// } 
+// console.log(sums);
+// // 10
+
+// let sumss = 0
+// for(let k = 0;k<=100;k++)
+// {
+//     if(k%2==0){
+//         console.log('even');
+//         sumss=sumss+k
+//     }
+// }
+// console.log(sumss);
+
+// // 11
+// for(let l= 1;l<=2048;l*=2){
+//     if(l%2==0){
+//         console.log(l);
+//     }
+// }
+// //12
+// for(let mm = 1;mm<=100;mm++){
+//     console.log(mm);
+// }
+
+// // //
+// // 13
+
+// for(let m = 1;m<=100;m++){
+//     if(m%3==0&&m%5==0)
+//     console.log('fizzbuzz');
+//     console.log(m);
+// }
+// //14
+
+// //?
+
+// //15
+// for(let i = 1;i <=50;i++)
+// if(i<=10){
+//     console.log(i, " a ");
+// }else if(i<=20){
+//     console.log(i , "b");
+// }else if (i<=30){
+//     console.log(i , " c");
+// }else if(i<=40){
+//     console.log(i , "d");
+// }else if(i <=50){
+//     console.log(i , "e");
+// }else{
+//     console.log(i," sorry no match ")
+// }
+//16
+
+for(let nn = 50;nn>=1;nn--)
+if(nn>=40){
+    console.log(nn," e");
+}else if(nn>=30){
+    console.log(nn, " d");
+}else if(nn>=20){
+    console.log(nn, " c ");
+}else if(nn>=10){
+    console.log(nn, " b");
+}else{
+    console.log(nn, " a");
+}
+
+
+// //17// //?
+
+// //18
+
+// for(let x= 0;x<=500;x++){
+//     if(x%3===0)
+//     {
+//         console.log(x);
+//     }
+// }
+
+
+// //19
+
+// //?
+
+// //20
+// for(let y=100;y>=5;y--){
+//     if(y%5===0)
+//     {
+//         console.log(y);
+//     }
+// }
+
+
+// /// 21
+
+
+// for(let z = 0;z<=70;z++){
+//     if(z%7===0){
+//         console.log(z);
+//     }
+// }
+
+// //22
+
+// let aa = 1
+// while(aa<=10){
+//     console.log(aa);
+//     aa++
+// }
+// let sum=0
+// let bb =1
+// while(bb<11){
+//     if(bb%2==0)
+//     console.log(bb);
+//     bb++
+//     sum=sum+bb
+// }
+
+// //
+// let cc=0
+// while(cc<=20){
+//     if(cc%2==0)
+//     console.log(cc);
+//     cc++
+// }
+
+// //
+
+// let dd =0
+// while(dd<=100){
+// if(dd%3===0)
+// console.log(dd);
+// console.log("odd");
+// dd++
+// }
+//\\\////\\/\\/\/\\\/\\/\/\/\/\\/\/\/\/\/\/\/\\/\\\/\\//
+
